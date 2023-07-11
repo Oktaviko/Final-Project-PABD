00d6554c0c6a4587a6724e5b9660d0c7255fadb0
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2023-07-11 12:28:59 +0700

    tambah stringconnection ke sql pada formdatakereta

--- Final Project PABD/FormDataKereta.cs
+++ Final Project PABD/FormDataKereta.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Final_Project_PABD
 {
     public partial class FormDataKereta : Form
     {
+        private string stringconnection = "data source=LAPTOP-NAUFALAS\\NAUFALAS;" + "database=FinalPABD;user ID=sa;password=bahtera1234";
+        private SqlConnection koneksi;
         public FormDataKereta()
         {
             InitializeComponent();
