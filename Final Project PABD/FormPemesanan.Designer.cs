52430c24c413bfb6c341a8a217662c6fd02d2bf8
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-07-12 01:59:06 +0700

    add method save, btnadd, dan datagridview

--- Final Project PABD/FormPemesanan.Designer.cs
+++ Final Project PABD/FormPemesanan.Designer.cs
@@ -54,6 +54,7 @@
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
@@ -72,6 +73,7 @@
             this.btnSave.TabIndex = 2;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnClear
             // 
--- Final Project PABD/FormPemesanan.cs
+++ Final Project PABD/FormPemesanan.cs
@@ -62,5 +62,57 @@ namespace Final_Project_PABD
             dataGridView1.DataSource = dt;
             koneksi.Close();
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string id_pemesanan = tbxIDpesan.Text;
+            string id_tiket = cbxIDTiket.Text;
+            string nik = cbxNIK.Text;
+            string no_hp = cbxNohp.Text;
+
+            if (id_pemesanan == "" || id_tiket == "" || nik == "" || no_hp == "")
+            {
+                MessageBox.Show("Harap Masukkan Semua Data", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "insert into dbo.Pemesanan (id_pemesanan, id_tiket, nik, no_hp) VALUES (@id_pemesanan, @id_tiket, @nik, @no_hp)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("@id_pemesanan", id_pemesanan));
+                cmd.Parameters.Add(new SqlParameter("@id_tiket", id_tiket));
+                cmd.Parameters.Add(new SqlParameter("@nik", nik));
+                cmd.Parameters.Add(new SqlParameter("@no_hp", no_hp));
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                koneksi.Close();
+                dataGridView1_CellContentClick();
+                refreshform();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            tbxIDpesan.Enabled = true;
+            cbxIDTiket.Enabled = true;
+            cbxNIK.Enabled = true;
+            cbxNohp.Enabled = true;
+            btnClear.Enabled = true;
+            btnSave.Enabled = true;
+            btnAdd.Enabled = false;
+            
+        }
+        private void dataGridView1_CellContentClick()
+        {
+            koneksi.Open();
+            string str = "select * From dbo.Pemesanan";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
     }
 }
